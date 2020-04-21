6fe2a057415e06b9a798e8bf37baae655a8d19fe
Tim Sullivan <tisulliv@microsoft.com>
2020-04-21 16:01:12 -0500

    added param block to clean up script

--- automation/PowerShell/new-testnetwork.ps1
+++ automation/PowerShell/new-testnetwork.ps1
@@ -1,4 +1,17 @@
-$virtualNetowrk = New-AzVirtualNetwork -Name 'vnet-dns-t-01' -ResourceGroupName 'rg-dns-core-01' -Location 'East US 2' -AddressPrefix '10.0.0.0/16'
+[cmdletbinding()]
+param(
+  $ResourceGroup = 'rg-dns-core-01',
+  $Location = 'East US 2', 
+  $VirtualNetworkName = 'vnet-dns-t-01'
+)
+
+try {
+  get-azresourcegroup -ResourceGroupName $ResourceGroup -ErrorAction Stop
+} catch {
+  New-AzResourceGroup -Name $ResourceGroup -Location $Location
+}
+
+$virtualNetwork = New-AzVirtualNetwork -Name $VirtualNetworkName -ResourceGroupName  $ResourceGroup -Location $Location -AddressPrefix '10.0.0.0/16'
 
 $subnetConfig = Add-AzVirtualNetworkSubnetConfig `
   -Name default `
@@ -17,6 +30,6 @@ $subnetConfig = Add-AzVirtualNetworkSubnetConfig `
 
 $virtualNetwork | Set-AzVirtualNetwork
 
-$publicip = New-AzPublicIpAddress -ResourceGroupName 'rg-dns-core-01' -name "pip-azbastion" -location "eastus2" -AllocationMethod Static -Sku Standard
+$publicip = New-AzPublicIpAddress -ResourceGroupName  $ResourceGroup -name "pip-azbastion" -location $Location -AllocationMethod Static -Sku Standard
 
-$bastion = New-AzBastion -ResourceGroupName 'rg-dns-core-01' -Name "ab-01" -PublicIpAddress $publicip -VirtualNetworkName'vnet-dns-t-01' -VirtualNetworkRgName 'rg-dns-core-01'
+$bastion = New-AzBastion -ResourceGroupName  $ResourceGroup -Name "ab-01" -PublicIpAddress $publicip -VirtualNetworkName $VirtualNetworkName -VirtualNetworkRgName $ResourceGroup
